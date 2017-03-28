a6ec0e706ddc397ee52ced23dcc45de738f73603
Sygmei <Mizugola@gmail.com>
2017-03-28 17:27:20 +0200

    Create CanvasConfig.lua

---
+++ engine/Lib/StdLib/CanvasConfig.lua
@@ -0,0 +1,28 @@
+function DefaultValues(self)
+    self.x1 = 0;
+    self.y1 = 0;
+    self.x2 = 0;
+    self.y2 = 0;
+    self.x = 0;
+    self.y = 0;
+    self.width = 0;
+    self.height = 0;
+    self.rotation = 0;
+    self.layer = 0;
+    self.color = {
+        r = 255,
+        g = 255,
+        b = 255,
+        a = 255
+    }
+    self.origin = {
+        translation = {
+            x = 0,
+            y = 0
+        },
+        rotation = {
+            x = 0,
+            y = 0
+        }
+    }
+end
