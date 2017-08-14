3b9c317ca176cc8cf9d31f7113e91ea8c644eafe
Sygmei <Mizugola@gmail.com>
2017-08-14 19:58:32 +0200

    Add automatic API Import on GetHook

--- src/Script/Script.cpp
+++ src/Script/Script.cpp
@@ -1,25 +1,14 @@
-#include <SFML/Audio.hpp>
 #include <vili/Vili.hpp>
 
+#include <Bindings/Bindings.hpp>
 #include <Collision/PolygonalCollider.hpp>
 #include <Debug/Console.hpp>
 #include <Graphics/Canvas.hpp>
-#include <Graphics/LevelSprite.hpp>
 #include <Input/InputManager.hpp>
-#include <Scene/Camera.hpp>
 #include <Script/Script.hpp>
-#include <Sound/Music.hpp>
-#include <Sound/Sound.hpp>
 #include <System/Cursor.hpp>
-#include <System/Loaders.hpp>
-#include <System/Package.hpp>
-#include <System/Path.hpp>
-#include <Transform/UnitVector.hpp>
-#include <Triggers/Trigger.hpp>
 #include <Triggers/TriggerGroup.hpp>
 #include <Triggers/TriggerDatabase.hpp>
-#include <Types/Selectable.hpp>
-#include <Utils/MathUtils.hpp>
 #include <Utils/TypeUtils.hpp>
 
 namespace obe
@@ -34,15 +23,30 @@ namespace obe
             {
                 std::string hookType = containerMap[name].first;
                 if (hookType == Utils::Type::getClassType<Debug::Console*>())
+                {
+                    Bindings::Load(lua, "Core.Debug.Console");
                     (*lua)["Hook"][name] = containerMap[name].second->as<Debug::Console*>();
+                }
                 else if (hookType == Utils::Type::getClassType<System::Cursor*>())
+                {
+                    Bindings::Load(lua, "Core.System.Cursor");
                     (*lua)["Hook"][name] = containerMap[name].second->as<System::Cursor*>();
+                }
                 else if (hookType == Utils::Type::getClassType<Input::InputManager*>())
+                {
+                    Bindings::Load(lua, "Core.Input.InputManager");
                     (*lua)["Hook"][name] = containerMap[name].second->as<Input::InputManager*>();
+                }
                 else if (hookType == Utils::Type::getClassType<Triggers::TriggerDatabase*>())
+                {
+                    Bindings::Load(lua, "Core.Triggers.TriggerDatabase");
                     (*lua)["Hook"][name] = containerMap[name].second->as<Triggers::TriggerDatabase*>();
+                }
                 else if (hookType == Utils::Type::getClassType<Triggers::TriggerGroup*>())
+                {
+                    Bindings::Load(lua, "Core.Triggers.TriggerGroup");
                     (*lua)["Hook"][name] = containerMap[name].second->as<Triggers::TriggerGroup*>();
+                }
                 else
                     throw aube::ErrorHandler::Raise("ObEngine.Script.CoreHook.UnknownHookType", {{"type", hookType}, {"name", name}});
             }
