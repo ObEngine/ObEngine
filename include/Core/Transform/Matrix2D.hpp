ad05a99ee83a6febb6588f4477ebbdb36ec68bb0
TeddyTrqt <turquet.teddy@gmail.com>
2019-07-02 23:56:56 +0200

    Fix bug from issue #176 (#177)

---
+++ include/Core/Transform/Matrix2D.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <Transform/UnitVector.hpp>
+#include <array>
+
+namespace obe::Transform
+{
+    class Matrix2D
+    {
+    private:
+        std::array<double, 4> m_elements;
+    public:
+	    Matrix2D(std::array<double, 4> elements);
+        UnitVector product(UnitVector vector);
+    };
+}
