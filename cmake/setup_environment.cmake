afb1588dd66e5af6b4180c9713d662d190e22c60
Sygmei <Mizugola@gmail.com>
2019-12-07 01:48:10 +0100

    Setup is now easy on Windows

--- cmake/setup_environment.cmake
+++ cmake/setup_environment.cmake
@@ -13,31 +13,52 @@ function (copy_required_dlls TARGET)
 
     if (WIN32)
         add_custom_command(TARGET ${TARGET} POST_BUILD
-            COMMAND ("${CMAKE_COMMAND}" -E copy_if_different 
+            COMMAND ("${CMAKE_COMMAND}" -E copy_if_different
                 $<$<CONFIG:Debug>:"${dlls-sfml-audio-dbg}">
-                $<$<CONFIG:Release>:"${dlls-sfml-audio-rel}"> 
+                $<$<CONFIG:Release>:"${dlls-sfml-audio-rel}">
                 $<TARGET_FILE_DIR:${TARGET}>
             )
-            COMMAND ("${CMAKE_COMMAND}" -E copy_if_different 
+            COMMAND ("${CMAKE_COMMAND}" -E copy_if_different
                 $<$<CONFIG:Debug>:"${dlls-sfml-graphics-dbg}">
-                $<$<CONFIG:Release>:"${dlls-sfml-graphics-rel}"> 
+                $<$<CONFIG:Release>:"${dlls-sfml-graphics-rel}">
                 $<TARGET_FILE_DIR:${TARGET}>
             )
-            COMMAND ("${CMAKE_COMMAND}" -E copy_if_different 
+            COMMAND ("${CMAKE_COMMAND}" -E copy_if_different
                 $<$<CONFIG:Debug>:"${dlls-sfml-network-dbg}">
-                $<$<CONFIG:Release>:"${dlls-sfml-network-rel}"> 
+                $<$<CONFIG:Release>:"${dlls-sfml-network-rel}">
                 $<TARGET_FILE_DIR:${TARGET}>
             )
-            COMMAND ("${CMAKE_COMMAND}" -E copy_if_different 
+            COMMAND ("${CMAKE_COMMAND}" -E copy_if_different
                 $<$<CONFIG:Debug>:"${dlls-sfml-system-dbg}">
-                $<$<CONFIG:Release>:"${dlls-sfml-system-rel}"> 
+                $<$<CONFIG:Release>:"${dlls-sfml-system-rel}">
                 $<TARGET_FILE_DIR:${TARGET}>
             )
-            COMMAND ("${CMAKE_COMMAND}" -E copy_if_different 
+            COMMAND ("${CMAKE_COMMAND}" -E copy_if_different
                 $<$<CONFIG:Debug>:"${dlls-sfml-window-dbg}">
-                $<$<CONFIG:Release>:"${dlls-sfml-window-rel}"> 
+                $<$<CONFIG:Release>:"${dlls-sfml-window-rel}">
                 $<TARGET_FILE_DIR:${TARGET}>
             )
         )
+        get_filename_component(SFML_BACK_DIR ${SFML_DIR} DIRECTORY)
+        get_filename_component(SFML_BACK_BACK_DIR ${SFML_BACK_DIR} DIRECTORY)
+        get_filename_component(SFML_BACK_BACK_BACK_DIR ${SFML_BACK_BACK_DIR} DIRECTORY)
+        find_file(
+            OPENAL_DLL
+            NAMES "openal32.dll"
+            PATHS
+                ${SFML_DIR}/bin
+                ${SFML_BACK_BACK_BACK_DIR}/bcxx
+        )
+        if (OPENAL_DLL)
+            add_custom_command(
+                TARGET ${TARGET} POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy
+                        ${OPENAL_DLL}
+                        $<TARGET_FILE_DIR:${TARGET}>/openal32.dll
+            )
+            message("Found OpenAL DLL : ${OPENAL_DLL}")
+        else()
+            message("OpenAL DLL not found, manual copy might be required")
+        endif()
     endif()
 endfunction()
