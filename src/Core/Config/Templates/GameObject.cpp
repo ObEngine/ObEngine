2cf39463c50285ad1826d8abdc5bc92dceb4d585
Sygmei <3835355+Sygmei@users.noreply.github.com>
2020-06-14 15:54:32 +0200

    Better exception when encountering error in Scene::onLoadCallback

--- src/Core/Config/Templates/GameObject.cpp
+++ src/Core/Config/Templates/GameObject.cpp
@@ -13,6 +13,17 @@ namespace obe::Config::Templates
         state.push_template("ViewPercentage", "ViewPercentage");
         state.push_template("ViewPixels", "ViewPixels");
 
+        // Referentials
+        state.push_template("TopLeft", "TopLeft");
+        state.push_template("TopLeft", "Top");
+        state.push_template("TopRight", "TopRight");
+        state.push_template("Left", "Left");
+        state.push_template("Center", "Center");
+        state.push_template("Right", "Right");
+        state.push_template("BottomLeft", "BottomLeft");
+        state.push_template("Bottom", "Bottom");
+        state.push_template("BottomRight", "BottomRight");
+
         // Sprite
         vili::node rect = vili::object { { "x", 0.f }, { "y", 0.f }, { "width", 0.f },
             { "height", 0.f }, { "unit", "SceneUnits" } };
