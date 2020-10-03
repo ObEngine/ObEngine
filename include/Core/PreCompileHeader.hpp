c649984c30737060b78d26f6cd13474d2d9b54c7
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2020-10-03 16:03:07 +0200

    Reduce compile time through PCH and parallel builds (#317)

---
+++ include/Core/PreCompileHeader.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <chrono>
+#include <exception>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <sol/sol.hpp>
+#include <spdlog/spdlog.h>
+#include <vili/node.hpp>
