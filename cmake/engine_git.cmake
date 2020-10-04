67bc173e04ff0271f97aeabdc413de7dc835828b
Kevin Leonardic <obengine@leonardic.de>
2020-10-04 15:33:59 +0200

    Make version object available through obe::Config::OBENGINE_VERSION_OBJECT (#294)

--- cmake/engine_git.cmake
+++ cmake/engine_git.cmake
@@ -1,7 +1,42 @@
-function (set_git_version BRANCH HASH VERSION)
+function (set_git_version BRANCH HASH VERSION_UNFORMATTED)
     set(OBENGINE_GIT_BRANCH ${BRANCH} CACHE STRING "ObEngine git branch" FORCE)
     set(OBENGINE_GIT_COMMIT_HASH ${HASH} CACHE STRING "ObEngine git commit hash" FORCE)
-    set(OBENGINE_VERSION ${VERSION} CACHE STRING "ObEngine version" FORCE)
+
+    string(REGEX MATCH "v(.+)\.(.+)\.(.+)$" VERSION "${VERSION_UNFORMATTED}")
+
+    set(OBENGINE_VERSION ${VERSION} CACHE STRING "ObEngine version string" FORCE)
+    set(VERSION_MAJOR ${CMAKE_MATCH_1})
+    set(VERSION_MINOR ${CMAKE_MATCH_2})
+    set(VERSION_PATCH ${CMAKE_MATCH_3})
+
+    string(REGEX MATCH "([0-9]+)$" RES ${VERSION_MAJOR})
+    if(NOT ${RES} STREQUAL "")
+        set(VERSION_MAJOR ${CMAKE_MATCH_1})
+        # 0*([0-9]+)$ seems nicer, but CMake can not lazily match
+        math(EXPR VERSION_MAJOR "${VERSION_MAJOR} * 1")
+    else()
+        set(VERSION_MAJOR "-1")
+    endif()
+
+    string(REGEX MATCH "([0-9]+)$" RES ${VERSION_MINOR})
+    if(NOT ${RES} STREQUAL "")
+        set(VERSION_MINOR ${CMAKE_MATCH_1})
+        math(EXPR VERSION_MINOR "${VERSION_MINOR} * 1")
+    else()
+        set(VERSION_MINOR "-1")
+    endif()
+
+    string(REGEX MATCH "([0-9]+)$" RES ${VERSION_PATCH})
+    if(NOT ${RES} STREQUAL "")
+        set(VERSION_PATCH ${CMAKE_MATCH_1})
+        math(EXPR VERSION_PATCH "${VERSION_PATCH} * 1")
+    else()
+        set(VERSION_PATCH "-1")
+    endif()
+
+    set(OBENGINE_VERSION_MAJOR ${VERSION_MAJOR} CACHE STRING "ObEngine major version" FORCE)
+    set(OBENGINE_VERSION_MINOR ${VERSION_MINOR} CACHE STRING "ObEngine minor version" FORCE)
+    set(OBENGINE_VERSION_PATCH ${VERSION_PATCH} CACHE STRING "ObEngine patch version" FORCE)
 endfunction()
 
 function (configure_obengine_git)
@@ -40,9 +75,8 @@ function (configure_obengine_git)
                 OUTPUT_VARIABLE GIT_OBENGINE_VERSION_UNFORMATTED
                 OUTPUT_STRIP_TRAILING_WHITESPACE
             )
-            string(REGEX MATCH "v(.+)\.(.+)\.(.+)$" GIT_OBENGINE_VERSION "${GIT_OBENGINE_VERSION_UNFORMATTED}")
 
-            set_git_version("${GIT_BRANCH}" "${GIT_COMMIT_HASH}" "${GIT_OBENGINE_VERSION}")
+            set_git_version("${GIT_BRANCH}" "${GIT_COMMIT_HASH}" "${GIT_OBENGINE_VERSION_UNFORMATTED}")
             message(STATUS "ÖbEngine Git version : ${OBENGINE_GIT_BRANCH}:${OBENGINE_GIT_COMMIT_HASH}")
             message(STATUS "ÖbEngine version : ${OBENGINE_VERSION}")
         else()
