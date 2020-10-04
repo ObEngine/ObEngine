6025aec63b1e03a1d74ebaa5005fe4cfe2a2a7f1
Kevin Leonardic <obengine@leonardic.de>
2020-10-04 16:01:54 +0200

    Make sure version information is up-to-date without reconfigure (#294)

--- cmake/engine_git.cmake
+++ cmake/engine_git.cmake
@@ -2,7 +2,7 @@ function (set_git_version BRANCH HASH VERSION_UNFORMATTED)
     set(OBENGINE_GIT_BRANCH ${BRANCH} CACHE STRING "ObEngine git branch" FORCE)
     set(OBENGINE_GIT_COMMIT_HASH ${HASH} CACHE STRING "ObEngine git commit hash" FORCE)
 
-    string(REGEX MATCH "v(.+)\.(.+)\.(.+)$" VERSION "${VERSION_UNFORMATTED}")
+    string(REGEX MATCH "v(.+)\\.(.+)\\.(.+)$" VERSION "${VERSION_UNFORMATTED}")
 
     set(OBENGINE_VERSION ${VERSION} CACHE STRING "ObEngine version string" FORCE)
     set(VERSION_MAJOR ${CMAKE_MATCH_1})
@@ -39,22 +39,21 @@ function (set_git_version BRANCH HASH VERSION_UNFORMATTED)
     set(OBENGINE_VERSION_PATCH ${VERSION_PATCH} CACHE STRING "ObEngine patch version" FORCE)
 endfunction()
 
-function (configure_obengine_git)
-    find_package(Git QUIET)
+function (configure_obengine_git GIT_EXECUTABLE ObEngine_SOURCE_DIR)
     message(STATUS "Configure Git variables for ÖbEngine")
-    if (Git_FOUND)
+    if (NOT GIT_EXECUTABLE STREQUAL "")
         execute_process(
             COMMAND ${GIT_EXECUTABLE} rev-parse --is-inside-work-tree
-            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            WORKING_DIRECTORY ${ObEngine_SOURCE_DIR}
             OUTPUT_VARIABLE IS_CMAKE_SOURCE_DIR_A_GIT_DIRECTORY
             OUTPUT_STRIP_TRAILING_WHITESPACE
             ERROR_VARIABLE _testOut
         )
-        if (${IS_CMAKE_SOURCE_DIR_A_GIT_DIRECTORY})
+        if (${IS_CMAKE_SOURCE_DIR_A_GIT_DIRECTORY} STREQUAL "true")
             # Get the git branch name
             execute_process(
                 COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
-                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                WORKING_DIRECTORY ${ObEngine_SOURCE_DIR}
                 OUTPUT_VARIABLE GIT_BRANCH
                 OUTPUT_STRIP_TRAILING_WHITESPACE
             )
@@ -62,7 +61,7 @@ function (configure_obengine_git)
             # Get the latest abbreviated commit hash of the working branch
             execute_process(
                 COMMAND ${GIT_EXECUTABLE} log -1 --format=%H
-                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                WORKING_DIRECTORY ${ObEngine_SOURCE_DIR}
                 OUTPUT_VARIABLE GIT_COMMIT_HASH
                 OUTPUT_STRIP_TRAILING_WHITESPACE
             )
@@ -71,7 +70,7 @@ function (configure_obengine_git)
             # Get the version tag from git
             execute_process(
                 COMMAND ${GIT_EXECUTABLE} for-each-ref refs/tags --sort=-taggerdate --count=1
-                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                WORKING_DIRECTORY ${ObEngine_SOURCE_DIR}
                 OUTPUT_VARIABLE GIT_OBENGINE_VERSION_UNFORMATTED
                 OUTPUT_STRIP_TRAILING_WHITESPACE
             )
@@ -90,3 +89,10 @@ function (configure_obengine_git)
         set_git_version("unknown" "unknown" "v?.?.?")
     endif()
 endfunction()
+
+configure_obengine_git(${GIT_EXECUTABLE} ${ObEngine_SOURCE_DIR})
+
+configure_file(
+    "${ObEngine_SOURCE_DIR}/include/Core/Config/Git.hpp.in"
+    "${ObEngine_BINARY_DIR}/include/Core/Config/Git.hpp"
+)
