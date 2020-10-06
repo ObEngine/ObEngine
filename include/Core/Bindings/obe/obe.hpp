edcad0cb79accb3eee0fcbd47c7ec1cfbb3a501a
Sygmei <3835355+Sygmei@users.noreply.github.com>
2020-10-06 11:39:16 +0200

    Removed useless function bindings

--- include/Core/Bindings/obe/obe.hpp
+++ include/Core/Bindings/obe/obe.hpp
@@ -8,6 +8,5 @@ namespace obe::Bindings
 {
     void LoadClassDebugInfo(sol::state_view state);
     void LoadClassException(sol::state_view state);
-    void LoadFunctionDOSOMETHING(sol::state_view state);
     void LoadFunctionInitEngine(sol::state_view state);
 };
