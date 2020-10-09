795304ed1a7f99f6bdb82949e7fcf0d0f3f5a1d4
Terens Tare <terens.t17@gmail.com>
2020-10-09 22:12:39 +0200

    (Hopefully) Finished vcpkg integration

--- cmake/check_vcpkg.cmake
+++ cmake/check_vcpkg.cmake
@@ -1,17 +1,18 @@
-function(obe_check_vcpkg)
+function(obe_check_vcpkg var)
     option(USE_VCPKG "Use vcpkg to handle some of the dependencies" OFF)
     if(DEFINED ENV{VCPKG_ROOT} AND NOT DEFINED CMAKE_TOOLCHAIN_FILE
         OR USE_VCPKG)
-        set(OBE_USE_VCPKG ON)
+        set(${var} ON PARENT_SCOPE)
         set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
             CACHE STRING "")
     endif()
 
-    if(OBE_USE_VCPKG)
+    if(${var})
         if(NOT DEFINED ENV{VCPKG_FEATURE_FLAGS})
-            set(ENV{VCPKG_FEATURE_FLAGS} "manifests") # manifests are required because of vcpkg.json
+        message(FATAL_ERROR
+            "Using vcpkg to build ObEngine without defining ${VCPKG_FEATURE_FLAGS} to be \"manifests\"!! Aborting...") # manifests are required because of vcpkg.json
         endif()
-            
+
         if(NOT DEFINED ENV{VCPKG_DEFAULT_TRIPLET})
             message(FATAL_ERROR
                 "Using vcpkg to build ObEngine without defining ${VCPKG_DEFAULT_TRIPLET}!! Aborting...")
