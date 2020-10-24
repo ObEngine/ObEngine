7ba38f924b13e55586c79f52d0d7a72a79c1afde
Sygmei <3835355+Sygmei@users.noreply.github.com>
2020-10-24 02:14:45 +0200

    Updated vili

--- extlibs/vili/src/parser/parser_state.cpp
+++ extlibs/vili/src/parser/parser_state.cpp
@@ -26,7 +26,7 @@ namespace vili::parser
 
     void state::set_indent(int64_t indent)
     {
-        if (m_indent_current == -1 && indent > 0)
+        if (m_indent_base == -1 && indent > 0)
         {
             m_indent_base = indent;
         }
@@ -35,27 +35,30 @@ namespace vili::parser
             throw exceptions::inconsistent_indentation(
                 indent, m_indent_base, VILI_EXC_INFO);
         }
-        indent /= m_indent_base; // Normalize indentation to "levels"
-        if (m_indent_current > indent)
+        if (m_indent_base > 0)
         {
-            for (auto decrease_indent = m_indent_current; decrease_indent > indent;
-                 decrease_indent--)
+            indent /= m_indent_base; // Normalize indentation to "levels"
+            if (m_indent_current > indent)
+            {
+                for (auto decrease_indent = m_indent_current; decrease_indent > indent;
+                     decrease_indent--)
+                {
+                    this->close_block();
+                }
+            }
+            else if (m_indent_current == indent && indent < m_stack.top().indent)
             {
                 this->close_block();
             }
-        }
-        else if (m_indent_current == indent && indent < m_stack.top().indent)
-        {
-            this->close_block();
-        }
-        else if (m_indent_current < indent)
-        {
-            if (indent - m_indent_current > 1 || indent > m_stack.top().indent)
+            else if (m_indent_current < indent)
             {
-                throw exceptions::too_much_indentation(indent, VILI_EXC_INFO);
+                if (indent - m_indent_current > 1 || indent > m_stack.top().indent)
+                {
+                    throw exceptions::too_much_indentation(indent, VILI_EXC_INFO);
+                }
             }
+            m_indent_current = indent;
         }
-        m_indent_current = indent;
     }
 
     void state::use_indent()
