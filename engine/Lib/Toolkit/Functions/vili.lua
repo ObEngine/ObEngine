d677556d192a7e0591a00e3d961c2ce637abd4de
Sygmei <3835355+Sygmei@users.noreply.github.com>
2023-04-10 03:46:54 +0200

    [feat] added from_json and to_json options on vili toolkit command

--- engine/Lib/Toolkit/Functions/vili.lua
+++ engine/Lib/Toolkit/Functions/vili.lua
@@ -1,5 +1,9 @@
 local Commands = require("obe://Lib/Toolkit/Commands");
 local dot_access = require("obe://Lib/StdLib/DotAccess").dot_access;
+---@module "Lib.Extlibs.json"
+local json = require("extlibs://json");
+local Color = require("Lib/StdLib/ConsoleColor");
+local Style = require("Lib/Toolkit/Stylesheet");
 
 local function _parse_(filename)
     print(vili.dump(vili.from_file(filename)));
@@ -26,6 +30,28 @@ local function _edit_(filename, getter, newvalue)
     vili.to_file(filename, data);
 end
 
+local function _from_json_(json_filepath)
+    local json_fd <close> = io.open(json_filepath, "r");
+    if not json_fd then
+        Color.print({
+            { text = "Could not open json file at path '", color = Style.Error},
+            { text = json_filepath, color = Style.Argument},
+            { text = "'", color = Style.Error},
+        });
+        return
+    end
+    local json_content = json_fd:read("*a");
+    local data = json.decode(json_content);
+    local vili_content = vili.to_string(data);
+    print(vili_content);
+end
+
+local function _to_json_(vili_filepath)
+    local content = vili.from_file(vili_filepath);
+    local json_content = json.encode(content);
+    print(json_content);
+end
+
 return {
     parse = Commands.command {
         Commands.help "Parses a vili document",
@@ -49,6 +75,20 @@ return {
                     Commands.call(_edit_)
                 }
             }
+        }
+    },
+    from_json = Commands.command {
+        Commands.help "Converts a JSON file to a Vili file",
+        json_filepath = Commands.arg {
+            Commands.help "Path to the JSON file to convert",
+            Commands.call(_from_json_)
+        }
+    },
+    to_json = Commands.command {
+        Commands.help "Converts a Vili file to a JSON file",
+        vili_filepath = Commands.arg {
+            Commands.help "Path to the Vili file to convert",
+            Commands.call(_to_json_)
         }
     }
 };
