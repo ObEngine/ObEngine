8ddfb88e2302605d07d5a5e3a89137ac116b836f
Sygmei <3835355+Sygmei@users.noreply.github.com>
2023-05-10 19:30:04 +0200

    test: added new bindings

--- src/Core/Collision/Trajectory.cpp
+++ src/Core/Collision/Trajectory.cpp
@@ -90,10 +90,7 @@ namespace obe::collision
         return m_reachable_collider_acceptor;
     }
 
-    void Trajectory::set_reachable_collider_acceptor(
-        const ReachableColliderAcceptor& 
-        acceptor
-        )
+    void Trajectory::set_reachable_collider_acceptor(const ReachableColliderAcceptor& acceptor)
     {
         m_reachable_collider_acceptor = acceptor;
     }
